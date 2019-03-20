44b15f058fa74aa663e1f44003ba5066b194fcd4
Theophile R <rotoudjimaye.theo@gmail.com>
2019-03-20 09:33:52 +0100

    Add support for HTTP proxies

--- lenddo_api_client/compat.py
+++ lenddo_api_client/compat.py
@@ -8,8 +8,9 @@ is_py3 = (ver[0] == 3)  # python 3.x
 
 if is_py2:
     from urllib import quote
-    from urllib2 import HTTPHandler, HTTPSHandler, Request, build_opener
+    from urllib2 import HTTPHandler, HTTPSHandler, Request, build_opener, \
+        ProxyHandler
 elif is_py3:
     from urllib.parse import quote
     from urllib.request import HTTPHandler, HTTPSHandler, Request, \
-        build_opener
+        build_opener, ProxyHandler
